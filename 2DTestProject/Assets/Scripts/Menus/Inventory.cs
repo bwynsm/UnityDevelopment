4ec18827a92e169dad9fe2370d613566a83c2fe3
vahava <benjamin.wynsma@gmail.com>
2016-05-05 09:19:58 -0700

    Preparing to add inventory
    - nothing really changed yet

---
+++ 2DTestProject/Assets/Scripts/Menus/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+
+	// stores a list of objects
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 2DTestProject/Assets/Scripts/Menus/Item.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item 
+{
+	public int strength;
+	public int intelligence;
+	public int luck;
+	public int stamina;
+	public int health;
+
+	public string command;
+	public string name;
+	public string description;
+	public Image itemImage;
+	public string restrictedClass;
+	public string requirements;
+
+
+
+}
